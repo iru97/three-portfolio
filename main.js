23c22d95a105bc02d57c04c8645b60ae3330639c
Iru <bubi@MacBook-Pro-de-Iru-6.local>
2024-02-18 15:28:39 +0000

    add dat.gui to modify values and add orbit controls

--- main.js
+++ main.js
@@ -1,22 +1,60 @@
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const gui = new dat.GUI();
+const world = {
+    plane: {
+        width: 10,
+        height: 10,
+        widthSegments: 10,
+        heightSegments: 10
+    }
+};
+gui.add(world.plane, 'width', 1, 20).
+    onChange(() => {
+        generatePlane();
+    }); 
+gui.add(world.plane, 'height', 1, 20).
+    onChange(() => {
+        generatePlane();
+    }); 
+gui.add(world.plane, 'widthSegments', 1, 20).
+    onChange(() => {
+        generatePlane();
+    }); 
+gui.add(world.plane, 'heightSegments', 1, 20).
+    onChange(() => {
+        generatePlane();
+    }); 
+
 const scene = new THREE.Scene();
 const camera = new THREE
     .PerspectiveCamera(75, innerWidth/ innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(innerWidth, innerHeight);
+
+/* The line `renderer.setPixelRatio(devicePixelRatio);` in the provided JavaScript code snippet is
+setting the pixel ratio of the renderer to match the device pixel ratio. This is important for
+ensuring that the rendering output is displayed correctly on devices with different pixel densities,
+such as high-DPI displays. */
 renderer.setPixelRatio(devicePixelRatio);
+
+camera.aspect = window.innerWidth / window.innerHeight;
+
+const controls = new OrbitControls( camera, renderer.domElement );
 
 document.body.appendChild(renderer.domElement);
 camera.position.z = 5;
 
 const planeGeometry = new THREE
     .PlaneGeometry(
-        5, 5, 10, 10
+        10, 10, 10, 10
     );
 const planeMaterial = new THREE
     .MeshPhongMaterial({
-        color: 0xff0000,
+        color: 0x6495ED,
         side: THREE.DoubleSide,
         flatShading: true
     });
@@ -24,15 +62,8 @@ const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
 
 scene.add(planeMesh);
 
-const { array: planeMeshPositions } = planeMesh.geometry.attributes.position;
-for (let i = 0; i < planeMeshPositions.length; i+= 3) {
-    const x = planeMeshPositions[i];
-    const y = planeMeshPositions[i + 1];
-    const z = planeMeshPositions[i + 2 ];
+setVerticesPosition();
 
-    planeMeshPositions[i + 2] = z + Math.random()
-    
-}
 const light = new THREE.DirectionalLight(
     0xffffff, 1
 );
@@ -41,11 +72,54 @@ scene.add(light);
 /* const ambientLight = new THREE.AmbientLight(
     0xffffff, 1
 ); */
+controls.update();
 
+/**
+ * The `animate` function uses `requestAnimationFrame` to continuously render the scene with the camera
+ * and potentially update the rotation of a plane mesh.
+ */
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     //planeMesh.rotation.x += 0.01;
 }
+
+function generatePlane() {
+    planeMesh.geometry.dispose();
+    planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, 10, 10)
+    setVerticesPosition();
+}
+
+function setVerticesPosition() {
+    /* This code snippet is iterating over the positions of vertices in the geometry of a plane mesh
+    (`planeMesh`) and modifying the z-coordinate of each vertex by adding a random value to it. Here's a
+    breakdown of what it's doing: */
+    const { array: planeMeshPositions } = planeMesh.geometry.attributes.position;
+    for (let i = 0; i < planeMeshPositions.length; i+= 3) {
+        const x = planeMeshPositions[i];
+        const y = planeMeshPositions[i + 1];
+        const z = planeMeshPositions[i + 2 ];
+
+        planeMeshPositions[i + 2] = z + Math.random()
+        
+    }
+}
+
+/**
+ * The function `onWindowResize` adjusts the camera aspect ratio, updates the projection matrix,
+ * resizes the renderer, sets the pixel ratio, and updates the controls.
+ */
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	//camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(devicePixelRatio);
+    controls.update();
+
+	animate()
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
 animate();
 /* console.log(scene, camera, renderer) */
